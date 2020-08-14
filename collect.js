2c07b4c2d1f4345db93fd83be23a3f36f120ee5c
Paul Downey <psd@whatfettle.com>
2020-08-14 20:00:09 +0100

    Gave broken parliamentary links an end-date

--- collect.js
+++ collect.js
@@ -4,12 +4,16 @@ const childProcess = require('child_process')
 
 const dataset = fs.readFileSync('./dataset/boundaries.csv')
 
+const today = new Date().toISOString().substring(0, 10)
+
 parse(dataset, {
   columns: true,
   on_record (row) {
     const splitUrl = row['resource-url'].split('.')
-    fs.mkdirSync(`./collection/${row.type.split('/')[0]}`, { recursive: true })
-    childProcess.exec(`curl ${row['resource-url']} > collection/${row.type}.${splitUrl[splitUrl.length - 1]}`)
+    if (row['end-date'].length == 0 || row['end-date'] > today) {
+        fs.mkdirSync(`./collection/${row.type.split('/')[0]}`, { recursive: true })
+        childProcess.exec(`curl ${row['resource-url']} > collection/${row.type}.${splitUrl[splitUrl.length - 1]}`)
+    }
     return row
   }
 })
